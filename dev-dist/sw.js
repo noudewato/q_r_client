7f6739d40b5862c09163ded0d7977cbbd4b954eb
noudewato <noudewatofawaz7@gmail.com>
2024-06-06 21:40:54 +0000

    new branch

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.b908scto7m8"
+    "revision": "0.qs3v082l3t8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
